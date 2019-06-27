7fed142f806fd8f132a52c5d7d25fb4412952faa
baymaxxjf <1340324168@qq.com>
2019-06-27 12:32:41 +0800

    update

--- bin/updateHub.bat
+++ bin/updateHub.bat
@@ -4,8 +4,10 @@ call git add *
 set /p comment="请输入代码变更说明:"
 if "%comment%"=="" ( 
     call git commit -m "update"
+    echo "默认代码变更说明"
 ) else (
     call git commit -m "%comment%"
+    echo "默认代码变更说明"
 )
 REM  这是注释
 REM  set  /p comment = "please input code change description"
