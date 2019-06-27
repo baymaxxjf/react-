f285a1ce94ced18e45c795fcfd456d2aaed7aae8
baymaxxjf <1340324168@qq.com>
2019-06-27 10:54:02 +0800

    aaa

--- bin/updateHub.bat
+++ bin/updateHub.bat
@@ -1,6 +1,8 @@
 call git status
 cd ..
 call git add *
-set /p comment="请输入代码变更说明:" 
+set /p comment="请输入代码变更说明:"
+REM  这是注释
+REM  set  /p comment = "please input code change description"
 call git commit -m "%comment%"
 call git push 
